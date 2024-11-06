b72af4fee0f96b5d36511bd25d3299cd0d01f424
Chenjo Prosper <chenjobober00@gmail.com>
2024-11-06 09:48:34 +0100

    Creation d'un code qr

--- services.py
+++ services.py
@@ -1,5 +1,11 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify,redirect, url_for, render_template
+import qrcode
+from io import BytesIO
+import base64
+import psycopg2
+from psycopg2.extras import RealDictCursor
 import math,requests
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -32,7 +38,6 @@ def calculate_distance_endpoint():
         return jsonify({'distance_km': distance}), 200
     except KeyError:
         return jsonify({'error': 'Invalid input. Make sure lat1, lon1, lat2, and lon2 are provided.'}), 400
-
 
 @app.route('/quartier-coordonnees', methods=['POST'])
 def quartier_coordonnees():
@@ -79,5 +84,56 @@ def quartier_coordonnees():
     except requests.exceptions.RequestException as e:
         return jsonify({"error": f"Erreur lors de la récupération des coordonnées : {str(e)}"}), 500
 
+
+conn = psycopg2.connect("dbname=prosper user=prosper password=prosper host=localhost")
+cursor = conn.cursor(cursor_factory=RealDictCursor)
+
+
+@app.route('/create_order', methods=['GET', 'POST'])
+def create_order():
+    if request.method == 'POST':
+        clientID = request.form['clientID']
+        prestataireID = request.form['prestataireID']
+        amount = request.form['amount']
+
+        if not clientID or not prestataireID:
+            return jsonify({"error":"donnees errone"}),400
+
+        # Obtenir la date et l'heure actuelles
+        current_datetime = datetime.now()
+
+        qr_data = f"Client: {clientID}, Prestataire:{prestataireID}, Amount: {amount}, date:{current_datetime}"
+        
+        # Génération du QR code
+        qr = qrcode.make(qr_data)
+        img_io = BytesIO()
+        qr.save(img_io, 'PNG')
+        img_io.seek(0)
+        qr_b64 = base64.b64encode(img_io.getvalue()).decode('utf-8')
+
+        # Enregistrer dans la base de données
+        cursor.execute(
+            "INSERT INTO orders (clientID, prestataireID, amount, qr_code) VALUES (%s, %s, %s, %s)",
+            (clientID, prestataireID, amount, qr_b64)
+        )
+        conn.commit()
+
+        return jsonify({
+            "code":qr_b64
+            }),200
+
+    return render_template('create_order.html')
+
+
+@app.route('/show_qr', methods=['GET', 'POST'])
+def show_qr():
+    if request.method == 'POST':
+        user_id = request.form['ClientID']
+        cursor.execute("SELECT * FROM orders WHERE clientID = %s", (user_id,))
+        orders = cursor.fetchall()
+        return render_template('show_qr.html', orders=orders)
+    return render_template('affiche_qr.html')
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='192.168.123.91',debug=True,port=5000)
