0557f88a2f5bba18d4e8516af9724bd7ba6561ab
Chenjo Prosper <chenjobober00@gmail.com>
2024-11-13 23:24:15 +0100

    Verification de code qr

--- services.py
+++ services.py
@@ -201,5 +201,58 @@ def scan_qr():
         # Si la méthode est GET, afficher une page HTML pour vérifier le QR code
         return render_template('verifier_qr.html')
 
+# Veification de code qr
+
+'''
+    Ce service utilise deux donnee envoyer en post:
+    - le code_qr(qr_code): qui est le text associer au code_qr scanner
+    - l'id du prestataire qui scanne le code(prestataireId)
+'''
+@app.route('/verification_qr',methods=['POST'])
+def verification():
+    if request.method == 'POST':
+        data = request.get_json()
+        qr_code = data.get('qr_code')
+        prestataireId = data.get('prestataireId')
+
+        try:
+            UUID(prestataireId)  # Vérifie que prestataireID est un UUID valide
+        except ValueError:
+            return jsonify({"error": "L'ID doit etre un UUID valide."}), 400
+
+        if prestataireId:
+            cursor.execute("SELECT * FROM orders WHERE qr_code = %s and etat = 'en cours'", (qr_code,)) # on recupere le code associer au qr
+            orders = cursor.fetchone()
+            if orders:
+                if orders['prestataireid'] == prestataireId: # On verifie si le prestataire est le bon
+                    cursor.execute("UPDATE orders SET etat = 'validé' WHERE id = %s", (orders['id'],))
+                    conn.commit()
+                    return jsonify(
+                        {
+                            "message": "Commande valide avec succès",
+                            "prestataireId":prestataireId,
+                            "amount":orders['amount']
+                        }
+                    ), 200
+                else:
+                    return jsonify({"message": "Le prestataire n'est pas le bon"}), 403
+            else:
+                return jsonify(
+                    {
+                        "message":"Code qr inconnu ou commande deja valider"
+                    }
+                )
+        else:
+            return jsonify(
+                {
+                    "message":"Le prestataire ne peut pas etre vide"
+                }
+            )
+    else:
+        return jsonify({
+            "message":"Mauvaise methode de request"
+        })
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True,port=5000)
