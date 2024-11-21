dcd0c9f3bd9d45fd9d8dce85c96634bb6c9e3814
Chenjo Prosper <chenjobober00@gmail.com>
2024-11-21 01:10:13 +0100

    depot des services sur le serveur

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 
 
-conn = psycopg2.connect("dbname=prosper user=prosper password=prosper host=localhost")
+conn = psycopg2.connect("dbname=food_team user=food_team password=food_team5 host=localhost")
 
 cursor = conn.cursor(cursor_factory=RealDictCursor)
 
