eae7c750fa3b09eb2da2203053c7d731e3f10418
Chenjo Prosper <chenjobober00@gmail.com>
2024-12-27 10:58:16 +0100

    Suppression des doublons

--- service_geolocalisation/app.py
+++ service_geolocalisation/app.py
@@ -9,8 +9,6 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 
-from flask_cors import CORS
-
 CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
 
 client = EurekaClient(
